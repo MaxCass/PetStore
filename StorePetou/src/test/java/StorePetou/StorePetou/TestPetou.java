cf1f9fb8287bd6b6c12587b7c28cac0e7c07438a
Formation <Formation@192.168.2.111>
2018-02-14 10:50:19 +0100

    Optimisation du TestPetou

---
+++ StorePetou/src/test/java/StorePetou/StorePetou/TestPetou.java
@@ -0,0 +1,45 @@
+package StorePetou.StorePetou;
+
+import org.junit.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TestPetou {
+	
+	
+	WebDriver driver;
+	
+	@Before
+	public void setup() {
+		
+		if(System.getProperty("navigateur").equalsIgnoreCase("firefox")) {
+			System.setProperty("webdriver.gecko.driver", "C:\\Users\\Formation\\Desktop\\Documents\\geckodriver.exe");
+			driver = new FirefoxDriver();
+		}
+		else if(System.getProperty("navigateur").equalsIgnoreCase("chrome")) {
+			System.setProperty("webdriver.chrome.driver", "C:\\Users\\Formation\\Desktop\\Documents\\chromedriver.exe");
+			driver = new ChromeDriver();
+		}	
+		
+		driver.manage().window().maximize();
+		driver.get("http://localhost:8082/jpetstore");
+	}
+	
+	@Test
+	public void testDuPetou() {
+		
+		driver.findElement(By.linkText("Enter the Store")).click();
+		driver.findElement(By.id("MainImage")).isDisplayed();
+		
+	}
+	
+	@After
+	public void teardown() {
+		
+		driver.quit();
+		
+	}
+}
+
