e66f95feaf80bf26b05e95310377fee178daab78
Formation <Formation@192.168.2.111>
2018-02-14 10:34:36 +0100

    Push initial

---
+++ src/main/java/StorePetou/StorePetou/App.java
@@ -0,0 +1,13 @@
+package StorePetou.StorePetou;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
